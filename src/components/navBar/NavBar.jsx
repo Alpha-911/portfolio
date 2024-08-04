7da63ecc6865599900b23855a6b051ef20a31d73
Rishu Raj <92977357+Alpha-911@users.noreply.github.com>
2024-08-04 23:45:13 +0530

    Update NavBar.jsx

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -12,7 +12,7 @@ function NavBar() {
     useEffect(() => {
         async function fetchData(){
             setIsLoading(true)
-            const response = await fetch('https://portfolio-server-27i3.onrender.com');
+            const response = await fetch('https://supreme-gerrie-rishu-raj-f3310073.koyeb.app/');
             const navBarData = await response.json();
             setPageData(navBarData.data[0].navBar);
 
